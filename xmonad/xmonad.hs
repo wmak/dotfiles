aa4c2d66b3ad594de45817b0187ec0d416982b1c
wmak <william.mak@hotmail.ca>
2014-02-09 02:55:01 -0500

    workaround for pipe issue with xmonad and xmobar

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,26 +1,20 @@
- import XMonad
- import XMonad.Hooks.DynamicLog
+import XMonad
+import XMonad.Hooks.ManageDocks
 
- main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
+main = xmonad $ defaultConfig
+	{ terminal    		= myTerminal
+	, modMask     		= myModMask
+	, borderWidth 		= myBorderWidth
+	, normalBorderColor 	= "#4CB09C"
+	, focusedBorderColor	= "#F04C16"
+	, layoutHook		= myLayout
+	, manageHook		= myManageHook
+	}
 
- myBar = "xmobar"
-
- -- Custom PP, configure it as you like. It determines what is being written to the bar.
- myPP = xmobarPP { 
-	ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" 
-     }
-
- -- Key binding to toggle the gap for the bar.
- toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
-
- myConfig = defaultConfig
-     { terminal    = myTerminal
-     , modMask     = myModMask
-     , borderWidth = myBorderWidth
-     }
-
- -- yes, these are functions; just very simple ones
- -- that accept no input and return static values
- myTerminal    = "urxvt"
- myModMask     = mod4Mask -- Win key or Super_L
- myBorderWidth = 3
+-- yes, these are functions; just very simple ones
+-- that accept no input and return static values
+myTerminal    = "urxvt"
+myModMask     = mod4Mask -- Win key or Super_L
+myBorderWidth = 3
+myLayout = avoidStruts $ layoutHook defaultConfig
+myManageHook = manageDocks <+> manageHook defaultConfig
