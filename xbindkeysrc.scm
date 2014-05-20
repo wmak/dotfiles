c6c9bd2b67b83b617fe588fd2d5c51e3675dc31d
wmak <william.mak@hotmail.ca>
2014-05-20 12:36:47 -0400

    adding new binding to lock computer

--- xbindkeysrc.scm
+++ xbindkeysrc.scm
@@ -1,3 +1,4 @@
 (xbindkey '("XF86AudioRaiseVolume") "amixer set Master 2dB+ unmute")
 (xbindkey '("XF86AudioLowerVolume") "amixer set Master 2dB- unmute")
 (xbindkey '("XF86Launch1") "google-chrome-stable &")
+(xbindkey '("XF86AudioMicMute") "slock")
