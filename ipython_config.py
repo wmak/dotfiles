2a5744d1245565ce53af2147518ddeb353f53c2f
William Mak <william@wmak.io>
2016-11-29 16:06:17 +0000

    Adding ipython config

---
+++ ipython_config.py
@@ -0,0 +1,33 @@
+#------------------------------------------------------------------------------
+# TerminalInteractiveShell configuration
+#------------------------------------------------------------------------------
+
+# Options for displaying tab completions, 'column', 'multicolumn', and
+# 'readlinelike'. These options are for `prompt_toolkit`, see `prompt_toolkit`
+# documentation for more information.
+c.TerminalInteractiveShell.display_completions = 'column'
+
+# The name of a Pygments style to use for syntax highlighting:  manni, igor,
+# lovelace, xcode, vim, autumn, vs, rrt, native, perldoc, borland, tango, emacs,
+# friendly, monokai, paraiso-dark, colorful, murphy, bw, pastie, algol_nu,
+# paraiso-light, trac, default, algol, fruity
+c.TerminalInteractiveShell.highlighting_style = 'vim'
+
+# Class used to generate Prompt token for prompt_toolkit
+from IPython.terminal.prompts import Prompts, Token
+class MyPrompt(Prompts):
+    print(dir(Prompts))
+    def in_prompt_tokens(self, cli=None):
+        return [
+                (Token.Prompt, 'In <'),
+                (Token.PromptNum, str(self.shell.execution_count)),
+                (Token.Prompt, '>: '),
+        ]
+
+    def out_prompt_tokens(self, cli=None):
+        return [
+                (Token.Prompt, 'Out<'),
+                (Token.PromptNum, str(self.shell.execution_count)),
+                (Token.Prompt, '>: '),
+        ]
+c.TerminalInteractiveShell.prompts_class = MyPrompt
